8eb00a35e6f6acc2f5974451574925ca9461dd94
Bob <bob.taffet@gmail.com>
2018-12-29 21:08:55 -0500

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+const obj = new ObjectID()
+console.log(obj)
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if(err) {
+		return console.log('Unable to connect to mongodb server')
+	}
+
+	console.log('Connected to MongoDB server')
+	// const db = client.db('TodoApp')
+
+	// db.collection('Todos').insertOne({
+	// 	text: 'Something to do',
+	// 	completed: false
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log('unable to instert Todo')
+	// 	}
+
+	// 	console.log(JSON.stringify(result.ops, undefined, 2))
+	// })
+
+	// db.collection('Users').insertOne({
+	// 	text: 'Bill',
+	// 	age: 67,
+	// 	location: 'Ulster Park'
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log('unable to instert User')
+	// 	}
+
+	// 	console.log(result.ops[0]._id.getTimestamp())
+	// })
+
+	client.close()
+})
---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if(err) {
+		return console.log('Unable to connect to mongodb server')
+	}
+	console.log('Connected to MongoDB server')
+	const db = client.db('TodoApp')
+
+    // db.collection('Todos').find({
+	// 	_id: new ObjectID('5c26faab29759f4eb9a2be71')
+	// }).toArray().then((docs) => {
+    //     console.log('Todos ')
+    //     console.log(JSON.stringify(docs, undefined, 2))
+    // }, (err) => {
+    //     console.log('Unable to fetch Todos', err)
+    // })
+
+	// db.collection('Todos').find({completed: true}).count((err, count) => {
+	// 	if(err) {
+	// 		console.log('Unable to fetch Todos', err)
+	// 	}
+	// 	console.log('Todos: ', count)
+	// })
+
+	db.collection('Users').find({location: 'Ulster Park'}).toArray().then((docs) => {
+		    console.log('Users:')
+		    console.log(JSON.stringify(docs, undefined, 2))
+		}, (err) => {
+		    console.log('Unable to fetch Users', err)
+		})
+	
+	client.close()
+})
