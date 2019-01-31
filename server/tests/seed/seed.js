33bed8c0016b5a325d291a4a46d0a628e09d47a8
Bob <bob.taffet@gmail.com>
2019-01-30 23:27:34 -0500

    Add tests for POST /users/login

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,25 +1,24 @@
-const {ObjectID} = require('mongodb')
-const jwt = require('jsonwebtoken')
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
 
-const {Todo} = require('./../../models/todo')
-const {User} = require('./../../models/user')
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
 
-const userOneID = new ObjectID()
-const userTwoID = new ObjectID()
-
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
 const users = [{
-	_id: userOneID,
+	_id: userOneId,
 	email: 'midnight@taffet.io',
 	password: 'passOne',
 	tokens: [{
 		access: 'auth',
-		token: jwt.sign({_id: userOneID, access: 'auth'}, 'abc123').toString()
+		token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
 	}]
 }, {
-	_id: userTwoID,
+	_id: userTwoId,
 	email: 'nala@taffet.io',
-	password: 'passTwo',
-}]
+	password: 'passTwo'
+}];
 
 const todos = [{
 	_id: new ObjectID(),
@@ -35,15 +34,15 @@ const populateTodos = (done) => {
 	Todo.remove({}).then(() => {
 		return Todo.insertMany(todos);
 	  }).then(() => done());
-}
+};
 
 const populateUsers = (done) => {
 	User.remove({}).then(() => {
-		const userOne = new User(users[0]).save()
-		const userTwo = new User(users[1]).save()
+		var userOne = new User(users[0]).save();
+		var userTwo = new User(users[1]).save();
 
 		return Promise.all([userOne, userTwo])
 	}).then(() => done());
-}
+};
 
-module.exports = {todos, populateTodos, users, populateUsers}
+module.exports = {todos, populateTodos, users, populateUsers};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
-const { ObjectID } = require('mongodb');
+const {ObjectID} = require('mongodb');
 
-const { app } = require('./../server');
-const { Todo } = require('./../models/todo');
-const { User } = require('./../models/user');
-const {todos, populateTodos, users, populateUsers} = require('./seed/seed')
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-beforeEach(populateUsers)
-beforeEach(populateTodos)
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
@@ -16,7 +16,7 @@ describe('POST /todos', () => {
 
 		request(app)
 			.post('/todos')
-			.send({ text })
+			.send({text})
 			.expect(200)
 			.expect((res) => {
 				expect(res.body.text).toBe(text);
@@ -26,7 +26,7 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 
-				Todo.find({ text }).then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -200,6 +200,7 @@ describe('/POST /users', () => {
 	it('should create a user', done => {
 		const email = 'example@example.com'
 		const password = '123mnb'
+
 		request(app)
 		.post('/users')
 		.send({email, password})
@@ -218,7 +219,7 @@ describe('/POST /users', () => {
 				expect(user).toBeTruthy()
 				expect(user.password).not.toBe(password)
 				done();
-			})
+			}).catch(e => done(e))
 		})
 	})
 
@@ -242,5 +243,56 @@ describe('/POST /users', () => {
 		.send({email, password})
 		.expect(400)
 		.end(done)
+	})
+})
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toBeTruthy();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				User.findById(users[1]._id).then((user) => {
+					expect(user.toObject().tokens[0]).toMatchObject({
+						access: 'auth',
+						token: res.headers['x-auth']
+					});
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should reject invalid login', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: 'abc234'
+			})
+			.expect(400)
+			.expect((res) => {
+				expect(res.headers['x-auth']).not.toBeTruthy();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
 	})
 })
