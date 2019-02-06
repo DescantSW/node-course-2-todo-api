39bad495d52995a6efa716524e5b222d21946df5
Bob <bob.taffet@gmail.com>
2019-02-06 00:57:51 -0500

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 
+if (env === 'development' || env === 'test') {
+	const config = require('./config.json')
+	const envConfig = config[env]
+
+	Object.keys(envConfig).forEach(key => {
+		process.env[key] = envConfig[key]
+	})
+}
+
 // Heroku set env to production console.count(label);
 // pacage.json script 'test' sets env to 'test'
 // server.js sets env to 'development' as a default
-if (env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-} else if (env === 'test') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-}
+// if (env === 'development') {
+// 	process.env.PORT = 3000;
+// 	process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+// } else if (env === 'test') {
+// 	process.env.PORT = 3000;
+// 	process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+// }
